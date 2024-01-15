fa7bcf9b0314ba740fee81176a6456198cf259d8
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-15 20:48:27 +0530

    adding files

---
+++ react fun facts/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import reactlogo from "../assets/react.svg"
+
+export default function Navbar() {
+    return (
+        <>
+            <header className="navbar">
+                <nav className="nav">
+                    <div className="nav-title">
+                        <img src={reactlogo} className="nav-logo" alt="react logo" />
+                        <h3>React Facts</h3>
+                    </div>
+                    <div>
+                        <h4 className="nav-text">React Course - Project 1</h4>
+                    </div>
+
+                </nav>
+            </header>
+        </>
+    )
+}
